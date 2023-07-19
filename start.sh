d10cde5a6be7f0ce96f7272b25be55b83fbe8c16
Aspect13 <ad13box@gmail.com>
2023-07-19 17:37:19 +0400

    testing build

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
 echo "Started"
-echo "Start python"
-python3 /results_collector.py
+#echo "Start python"
+#python3 /app/results_collector.py &
 echo "Start R"
-Rscript /results_processor.R
-echo "Start reporter"
-python3 /reporter.py
+Rscript /app/results_processor.R &
+#echo "Start reporter"
+#python3 /app/reporter.py &
